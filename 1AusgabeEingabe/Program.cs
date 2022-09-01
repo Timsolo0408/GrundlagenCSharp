afa0841468042f4021cc17b91b28ac78e3786648
Tim Hansonis <tim.h66@gso.schule.koeln>
2022-09-01 11:07:16 +0200

    Aufgabe 1 2 fertig und 3 angefangen

--- 1AusgabeEingabe/Program.cs
+++ 1AusgabeEingabe/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//A1
+//Console.WriteLine(" .-.");
+//Console.WriteLine("(o o)");
+//Console.WriteLine("| O |"); Console.WriteLine("|   |");
+//Console.WriteLine("'~~~'");
+
+//A2
+string Tim_Nachname = "Hansonis";
+string Tim_Vorname = "Tim";
+string Tim_Alter = "16";
+
+Console.WriteLine(Tim_Vorname);
+Console.WriteLine(Tim_Nachname);
+Console.WriteLine(Tim_Alter);
+
+//A3
+Console.WriteLine("Geben sie ihr alter an");
+Anonym_Alter = Console.ReadLine();
