ab0226de93a2e08a8f5722b2d318478093ed70bf
Tim Hansonis <tim.h66@gso.schule.koeln>
2022-09-08 11:05:01 +0200

    08.09.22 untericht aufgaben

--- 1AusgabeEingabe/Program.cs
+++ 1AusgabeEingabe/Program.cs
@@ -1,18 +1,44 @@
-﻿//A1
+﻿////A1
 //Console.WriteLine(" .-.");
 //Console.WriteLine("(o o)");
 //Console.WriteLine("| O |"); Console.WriteLine("|   |");
 //Console.WriteLine("'~~~'");
 
-//A2
-string Tim_Nachname = "Hansonis";
-string Tim_Vorname = "Tim";
-string Tim_Alter = "16";
+////A2
+//string Tim_Nachname = "Hansonis";
+//string Tim_Vorname = "Tim";
+//string Tim_Alter = "16";
 
-Console.WriteLine(Tim_Vorname);
-Console.WriteLine(Tim_Nachname);
-Console.WriteLine(Tim_Alter);
+//Console.WriteLine(Tim_Vorname);
+//Console.WriteLine(Tim_Nachname);
+//Console.WriteLine(Tim_Alter);
 
-//A3
-Console.WriteLine("Geben sie ihr alter an");
-Anonym_Alter = Console.ReadLine();
+////A3
+//Console.WriteLine("Geben sie ihr alter an");
+//Anonym_Alter = Console.ReadLine();
+
+////Arbeitsauftrag 2
+
+//string Vorname = "Tim";
+//int Alter = 16;
+//bool erste_programier_sprache = false;
+
+//Console.WriteLine(Vorname);
+//Console.WriteLine(Alter);
+//Console.WriteLine(erste_programier_sprache);
+
+////Arbeitsauftrag 3
+//int DollarZahl = 36;
+//char Dollar = (char)DollarZahl;
+//Console.WriteLine(Dollar);
+
+//Arbeitsauftrag 4
+Console.WriteLine("eben sie eine beliebige zahl ein");
+string varieable_1string = Console.ReadLine();
+short varieable_1short = Convert.ToInt16(varieable_1string);
+Console.WriteLine(varieable_1short);
+
+Console.WriteLine("eben sie eine beliebige zahl ein");
+string varieable_2string = Console.ReadLine();
+short varieable_2double = Convert.ToInt16(varieable_2string);
+Console.WriteLine(varieable_2double);
